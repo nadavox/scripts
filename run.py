db042584c97645422e2ad253c16ac7f4e4efb8b7
adi604 <84729170+adi604@users.noreply.github.com>
2024-07-05 15:03:30 +0300

    crate ustils + run files and update input as json for courses dictionary

---
+++ run.py
@@ -0,0 +1,25 @@
+from utils import cut_ogg_in_thirds, parser_json
+
+courses = {}
+
+def parse_args():
+    # import argparse
+    # parser = argparse.ArgumentParser(description="Convert an OGG audio file into words.")
+    # parser.add_argument('-f', '--input_file', type=str, help='Path where the OGG audio files are saved')
+    # parser.add_argument('-n', '--num_files', type=int, default=1, help='Number of files to process')
+    # parser.add_argument('-o', '--output_file', type=str, help='Paths to save the conclution of the audio file')
+    # return parser.parse_args()
+    import argparse
+    parser = argparse.ArgumentParser(description="Convert an OGG audio file into words.")
+    parser.add_argument('-f', '--input_file', type=str, help='Path to the JSON file')
+    parser.add_argument('-o', '--output_file', type=str, help='Path to save the conclution of the audio files')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    input_json = args.input_file
+    output_path = args.output_file
+
+    courses = parser_json(input_json)
+
+    cut_ogg_in_thirds(courses, output_path)
